4a155217e57c21da7bb16f2cc96ca0e69626f397
Linakretzsch <lina.kretzschmar@hotmail.de>
2020-10-11 17:59:15 -0400

    changed player according to cards

---
+++ player/Player.h
@@ -0,0 +1,78 @@
+//
+//  Player.h
+//  Player
+//
+//  Created by Lina Kretzschmar on 2020-09-27.
+//
+
+#pragma once
+
+#include "../Map/Map.h"
+#include "../Orders/Orders.h"
+#include "../Cards/Cards.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+/* Header_h */
+
+class Player
+{
+
+private:
+    static int playerCount; //Statinc counter, counting players of the game
+    unsigned int playerID;
+    string name;
+
+    list<shared_ptr<Territory>> territoryList;  //Territory list holding pointers to territories
+    Hand* hand; //A pointer to a vector list holding ptrs to warzone cards
+    OrderList* orderList;  //A pointer to a list holding ptrs to order objects
+
+public:
+
+    Player();
+    ~Player();
+    Player(string playerName);
+    Player(const Player&);
+    Player& operator=(const Player& aPlayer);
+    string to_string();
+    friend ostream& operator <<(ostream&, Player&);
+
+    void setName(string playerName);
+    string getName();
+    static int getPlayerCount();
+    unsigned int getPlayerID();
+
+
+    OrderList* getOrderList();
+    void issueOrder(string orderType);
+
+
+    list<shared_ptr<Territory>> getTerritoryList();
+    void addTerritory(shared_ptr<Territory> newTerritoryPtr);
+    string printList(list<shared_ptr<Territory>> aList);
+    list<shared_ptr<Territory>> toDefend(Map aMap);
+    list<shared_ptr<Territory>> toAttack(Map aMap);
+
+    Hand* getHand();
+
+};
+
+//*************      INSTRUCTIONS    ****************
+
+// Player owns a collection of territories
+
+// Players own a HandCards of Warzone cards
+
+// Player contains methods toDefend() and toAttack() that return a list of territories that are to be defended and to be attaked, respectively
+
+// Players contains an issueOrder() method that creates an order object and adds it to the list of orders
+
+// Driver creates players and demonstrates that the above features are available
+
+
+
---
+++ player/PlayerDriver.cpp
@@ -0,0 +1,120 @@
+//
+//  PlayerDriver.cpp
+//  Player
+//  The driver of the player class tests all classes implemented with the Player class. The player can own a collection of territories, a hand of warzone cards and a list of orders to be executed. In addition, the player can print all territories that can be attacked and all territories that can be defended as well as issueing an order and adding it to the list of orders.
+//  Created by Lina Kretzschmar on 2020-09-27.
+//
+
+
+#include <stdio.h>
+#include <iostream>
+#include "Player.h"
+
+using namespace std;
+
+int Player::playerCount = 0;
+
+
+
+int main() {
+
+    //Creating two players
+    Player player1("Berta");    //Created player1 using the paramterized constructor
+    Player player2;             //Created player2 with the default constructor
+    Player player3("Kevin");
+
+
+    //Testing copy constructor
+
+
+    //Testing stream insertion opertor
+
+
+
+    //Creating a map
+    Map testMap = Map();
+
+    //Creating two continents
+    shared_ptr<Continent> c1 = testMap.add(Continent(1, "America", 1));
+    shared_ptr<Continent> c2 = testMap.add(Continent(2, "Europe", 5));
+
+    //Creating new territories of type ptr and adding them to the testMap
+    shared_ptr<Territory> t1 = testMap.add(Territory(1, "Canada", 1));
+    shared_ptr<Territory> t2 = testMap.add(Territory(2, "Iceland", 2));
+    shared_ptr<Territory> t3 = testMap.add(Territory(3, "United Kingdom", 2));
+    shared_ptr<Territory> t4 = testMap.add(Territory(4, "France", 2));
+    shared_ptr<Territory> t5 = testMap.add(Territory(5, "Spain", 2));
+    shared_ptr<Territory> t6 = testMap.add(Territory(6, "Italy", 2));
+
+    //Connecting territories via directed graphs
+    testMap.link(t1, t2);
+    testMap.link(t2, t3);
+    testMap.link(t3, t4);
+    testMap.link(t4, t5);
+    testMap.link(t4, t1);
+    testMap.link(t5, t6);
+    testMap.link(t6, t3);
+    testMap.link(t6, t4);
+
+    //Printing the map
+    cout << "Testing map: \n" << testMap.to_string() << "\n";
+
+    //Adding two territories to player1
+    cout << "\nPrinting ownerId for t3: " << t3->ownerID;
+    player1.addTerritory(t3);
+    player1.addTerritory(t4);
+    cout << "\nPrinting ownerId for t3: " << t3->ownerID;
+
+
+
+    //Displaying territoryList containing territories currently owned by player2
+    cout << "\nTesting players list of territories:\n" + player1.printList(player1.getTerritoryList());
+
+    //Test issueOrder() creates an order objects and adds it to the OrderList
+    cout << "\nTest issueing orders:\n";
+
+    player1.issueOrder("Deploy");
+    player1.issueOrder("Advance");
+    player1.issueOrder("Bomb");
+    player1.issueOrder("Blockade");
+    player1.issueOrder("Airlift");
+    player1.issueOrder("Negotiate");
+
+    //Printing the list of orders
+    cout << "\nTest printing players list of orders" << player1.getName() << ":\n" << *(player1.getOrderList()) << endl;
+
+    //Test toAttack() which returns a list of territories that can be attacked
+    cout << "Test printing players territories to attack:\n" + player1.printList(player1.toAttack(testMap));
+
+    //Test toDefend()  which returns a list of territories that can be defended
+    cout << "\nTest printing players territories to defend:\n" + player1.printList(player1.toDefend(testMap));
+
+
+    //    cout << player1.printlist(player1.todefend(testmap)) << " printed todefend()";
+
+        //creating, initializing a deck of cards
+    Deck testDeck;
+    testDeck.initializeVecDeck(3);
+    //initialize_vec_deck();
+
+//test drawing two cards from the deck and adding them to the player hand and printing the hand
+    cout << "\ndrawing cards from the deck:\n";
+    /*player1.getHandOfCards()->addCard(testDeck.draw());
+    player1.getHandOfCards()->addCard(testDeck.draw());*/
+
+    player1.getHand()->addCard(testDeck.draw());
+    player1.getHand()->addCard(testDeck.draw());
+    //player1.getHand()->add_card_to_deck_vec(testDeck.draw()); prev version of code
+
+
+    cout << *(player1.getHand());
+
+    //testing players to_string method
+    cout << "\ntesting players tostring() method:" << player1.to_string() << endl;
+
+    //Test destructor
+
+
+    return 0;
+};
+
