acc92d27deb9aeb0858392741b37ef9906e8ebfc
LinaKretzsch <43006748+LinaKretzsch@users.noreply.github.com>
2020-10-11 23:18:24 -0400

    Update PlayerDriver.cpp

--- player/PlayerDriver.cpp
+++ player/PlayerDriver.cpp
@@ -15,28 +15,39 @@ using namespace std;
 int Player::playerCount = 0;
 
 
-
 int main() {
 
     //Creating two players
     Player player1("Berta");    //Created player1 using the paramterized constructor
     Player player2;             //Created player2 with the default constructor
-    Player player3("Kevin");
 
 
     //Testing copy constructor
+    Player* player3 (new Player("Trudy"));
+    Player* player4 = player3;
+    cout << "Testing copy constructor, printing name of player5 as example:\n" << player4->getName() << endl;
+
+
+    //Testing assignment operator --> This is currently not working. To get my code to work I currently have to comment out the Card destructor within the player destructor
+    //Player player5("Dennis");
+    //Player player6("Frida");
+    //player5 = player6; /*-->Cannot test right now since I need Hand's destructor*/
+    //cout << "\nTesting assignment operator, printing name of player 5 as example:\n" << player5.getName() << endl;
 
 
     //Testing stream insertion opertor
-
+    cout << "\nTesting stream insertion operator printing no values since player1 does not have any cards, territories or oders yet:";
+    cout << player1;
 
 
     //Creating a map
     Map testMap = Map();
 
+
     //Creating two continents
     shared_ptr<Continent> c1 = testMap.add(Continent(1, "America", 1));
     shared_ptr<Continent> c2 = testMap.add(Continent(2, "Europe", 5));
+
 
     //Creating new territories of type ptr and adding them to the testMap
     shared_ptr<Territory> t1 = testMap.add(Territory(1, "Canada", 1));
@@ -45,6 +56,7 @@ int main() {
     shared_ptr<Territory> t4 = testMap.add(Territory(4, "France", 2));
     shared_ptr<Territory> t5 = testMap.add(Territory(5, "Spain", 2));
     shared_ptr<Territory> t6 = testMap.add(Territory(6, "Italy", 2));
+
 
     //Connecting territories via directed graphs
     testMap.link(t1, t2);
@@ -56,23 +68,27 @@ int main() {
     testMap.link(t6, t3);
     testMap.link(t6, t4);
 
+
+    //Validating the map
+    cout << "Validating created map:\n";
+    testMap.validate();
+
+
     //Printing the map
-    cout << "Testing map: \n" << testMap.to_string() << "\n";
+    cout << "\nTesting created map: \n" << testMap.to_string() << "\n";
+
 
     //Adding two territories to player1
-    cout << "\nPrinting ownerId for t3: " << t3->ownerID;
     player1.addTerritory(t3);
     player1.addTerritory(t4);
-    cout << "\nPrinting ownerId for t3: " << t3->ownerID;
-
-
+   
 
     //Displaying territoryList containing territories currently owned by player2
     cout << "\nTesting players list of territories:\n" + player1.printList(player1.getTerritoryList());
 
+
     //Test issueOrder() creates an order objects and adds it to the OrderList
     cout << "\nTest issueing orders:\n";
-
     player1.issueOrder("Deploy");
     player1.issueOrder("Advance");
     player1.issueOrder("Bomb");
@@ -80,8 +96,10 @@ int main() {
     player1.issueOrder("Airlift");
     player1.issueOrder("Negotiate");
 
+
     //Printing the list of orders
-    cout << "\nTest printing players list of orders" << player1.getName() << ":\n" << *(player1.getOrderList()) << endl;
+    cout << "\nTest printing players list of orders " << player1.getName() << ":\n" << *(player1.getOrderList()) << endl;
+
 
     //Test toAttack() which returns a list of territories that can be attacked
     cout << "Test printing players territories to attack:\n" + player1.printList(player1.toAttack(testMap));
@@ -90,29 +108,21 @@ int main() {
     cout << "\nTest printing players territories to defend:\n" + player1.printList(player1.toDefend(testMap));
 
 
-    //    cout << player1.printlist(player1.todefend(testmap)) << " printed todefend()";
-
-        //creating, initializing a deck of cards
+    //creating, initializing a deck of cards
     Deck testDeck;
     testDeck.initializeVecDeck(3);
     //initialize_vec_deck();
 
-//test drawing two cards from the deck and adding them to the player hand and printing the hand
+
+    //test drawing two cards from the deck and adding them to the player hand and printing the hand
     cout << "\ndrawing cards from the deck:\n";
-    /*player1.getHandOfCards()->addCard(testDeck.draw());
-    player1.getHandOfCards()->addCard(testDeck.draw());*/
-
     player1.getHand()->addCard(testDeck.draw());
     player1.getHand()->addCard(testDeck.draw());
-    //player1.getHand()->add_card_to_deck_vec(testDeck.draw()); prev version of code
-
-
     cout << *(player1.getHand());
+
 
     //testing players to_string method
     cout << "\ntesting players tostring() method:" << player1.to_string() << endl;
-
-    //Test destructor
 
 
     return 0;
