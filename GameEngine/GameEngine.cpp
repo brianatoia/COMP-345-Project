d702ac1624773b7c2a19c9bfe8cb3c23ea7748b0
Linakretzsch <lina.kretzschmar@hotmail.de>
2020-11-07 17:21:04 -0500

    Created New Branch
    Create New Branch a2-GameEngine

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,4 +1,5 @@
 
+#include <iostream>
 #include "GameEngine.h"
 
 //*************		PART 1		**************//
@@ -7,47 +8,65 @@
 
 //*************		PART 2		**************//
 
-void addPlayers(Player* player)
+GameEngine::GameEngine()
 {
-	this->players->push_back(player);
+	this->players = list<shared_ptr<Player>>();
+	this->map = shared_ptr<Map>();
 }
 
-list <Player*> getPlayers()
+void GameEngine::addPlayers(shared_ptr<Player> player)
 {
-
+	players.push_back(player);
 }
 
 
-void startupPhase(Map* map, list<Player*> players)//Part 2
+list<shared_ptr<Player>> GameEngine::getPlayers()
+{
+	return players;
+}
+
+string GameEngine::getPlayerNames()
+{
+	string str = "";
+	for (auto player : players)
+		str += player->getName();
+}
+
+void GameEngine::startupPhase()//Part 2
 {
 	//1. The order of the players is determined randomly
-	players.random_shuffle();
+	random_shuffle(players.begin(), players.end());
 
 	//2. All territories in the map are randomly assigned to players one by one in round-robin fashion
+	
+	
 	//3. Players are given a number of initial armies
 }
 
 
+
 int main(){
-	//Declaring gameEngine
-	shared_ptr<GameEngine> gameEngine(new GameEngine());
+	////Declaring gameEngine
+	//shared_ptr<GameEngine> gameEngine(new GameEngine());
 
 
-	Player* player1 = new Player("Bettina");
-	Player* player2 = new Player("Sigmund");
-	players = new list <Player*>();
-	players->addPlayers(player1);
+	//shared_ptr<Player> p1 = shared_ptr<Player>();
+	//cout << p1 << endl;
+
+	////shared_ptr<Player> player2 = new Player("Sigmund");
+	/////*players = new list <Player*>();*/
+	////players->addPlayers(player1);
 
 
 
-	
-	//main game loop
-	//while (gameEngine->mainGameLoop())
-	//{
+	//
+	////main game loop
+	////while (gameEngine->mainGameLoop())
+	////{
 
-	//}
+	////}
 
-
+	cout << "Hello" << endl;
 
 
 
--- GameEngine/GameEngine.h
+++ GameEngine/GameEngine.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "../player/Player.h"
-//#include "../Cards/Cards.h"
-#include "Cards.h"//have to correct this with the correct folder structure after
+#include "../cards/Cards.h"
+//#include "Cards.h"//have to correct this with the correct folder structure after
 #include "../Map/Map.h"
 #include "../MapLoader/MapLoader.h"
 #include "../Orders/Orders.h"
@@ -21,18 +21,20 @@ public:
 	GameEngine& operator= (const GameEngine&);
 	friend ostream& operator<<(ostream& strm, GameEngine& gameEngine);
 
-	void addPlayers(Player* player);
-	list <shared_ptr<Player*>> getPlayers();
-	
+	void addPlayers(shared_ptr<Player> player);
+	list <shared_ptr<Player>> getPlayers();
+	string getPlayerNames();
 
-	void startupPhase(Map* map, list<shared_ptr<Player*>> playerList);	//Part 2
+
+	void startupPhase();	//Part 2
 
 	shared_ptr<GameEngine> createEngine();
 
 	bool mainGameLoop();
 
 private:
-	list<shared_ptr<Player*>> players;
+	shared_ptr<Map> map;
+	list<shared_ptr<Player>> players;
 	bool reinforcementsPhase();
 	bool issueOrdersPhase();
 	bool executeOrdersPhase();
